bb6845c8c882658049b17266726016d39ed33bfa
Amruth-111 <amruthkrishnaa@gmail.com>
2023-04-23 12:33:20 +0530

    exporting  and importing

--- server.js
+++ server.js
@@ -1,43 +1,9 @@
-let http=require('http');
-let fs=require('fs');
-let server=http.createServer((req,res)=>{
-    let url=req.url;
-    let method=req.method;
-    if(url==='/'){
-        fs.readFile("message.txt",{encoding: 'utf8'},(err, data)=>{
-            if(err){
-                    console.log(err);
-            } 
-            res.write(`<body>${data}</body>`)
-            res.write('<html><body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">submit</button></form></body></html>')
-            return res.end();
-        })
-        
-    }
-    
-    else if(url==='/message' && method==='POST'){
-        const body = [];
-        req.on('data', (chunk)=>{
-            body.push(chunk);
-            console.log(chunk);
-        })
-        return req.on('end', ()=>{
-            const response=Buffer.concat(body).toString()
-            console.log(response);
-            const message=response.split("=")[1]
-            
-            fs.writeFile('message.txt',message,err=>{
-            res.statusCode=302;
-            res.setHeader('location','/')
-            return res.end();
-            })
-            
-        })
-        
-    }
 
-   
-    
+let http = require('http');
+let routes=require('./routes');
 
-})
-server.listen(4000);
+console.log(routes.text)
+
+let server=http.createServer(routes.handler);
+
+server.listen(5000);

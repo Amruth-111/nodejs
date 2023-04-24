385eba7e0604dcbe97d456b55c74af24671154da
Amruth-111 <amruthkrishnaa@gmail.com>
2023-04-24 12:30:17 +0530

    first express server

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 
 let http = require('http');
-let routes=require('./routes');
+let express = require('express');
 
-console.log(routes.text)
+const app=express();
 
-let server=http.createServer(routes.handler);
+app.use((req,res,next)=>{
+    console.log("inside middleware1");
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("inside middleware2");
+    res.send('<h1>Hello world!</h1>');
+})
+
+let server=http.createServer(app);
+
 
 server.listen(5000);
